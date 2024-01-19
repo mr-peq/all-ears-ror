4f7437ed6845ea8488fba2e4cf2f4cff00ef6e8f
Samuel Paget <sjapaget@gmail.com>
2024-01-19 20:41:52 +0100

    cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'localhost:3000', '127.0.0.1:3000',
-    /\Ahttp:\/\/192\.168\.0\.\d{1,3}(:\d+)?\z/
+    /\Ahttp:\/\/192\.168\.0\.\d{1,3}(:\d+)?\z/,
+    'localhost:5173', '127.0.0.1:5173'
 
     resource "*",
       headers: :any,
