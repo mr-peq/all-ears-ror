c1dedfe7814ef45c12ebeb329a8524b31068afc1
Theodore Saulnier <theodore1712@yahoo.fr>
2024-02-13 19:25:16 +0100

    Added basic tests for users_controller#create

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -48,4 +48,54 @@ RSpec.describe UsersController, type: :controller do
       expect( response.parsed_body.keys ).to eq(expected_attributes)
     end
   end
+
+  describe "POST#create" do
+    it "creates 3 new users if provided 3 new nicknames" do
+      new_users_nicknames = ["julia", "bob", "fiona"]
+      post :create, params: { nicknames: new_users_nicknames }
+      response_users_nicknames = response.parsed_body.map { |user| user["nickname"] }
+      expect( response_users_nicknames ).to eq(new_users_nicknames)
+    end
+
+    it "creates users that don't yet exist and retrieves the ones that already exist" do
+      users_nicknames = [
+        allen.nickname,
+        joe.nickname,
+        sam.nickname,
+        peq.nickname,
+        "julia",
+        "bob",
+        "fiona"
+      ]
+      post :create, params: { nicknames: users_nicknames }
+      response_users_nicknames = response.parsed_body.map { |user| user["nickname"] }
+      expect( response_users_nicknames ).to eq(users_nicknames)
+    end
+
+    it "returns an error if no params key was provided" do
+      post :create, params: { fake_key: "fake key" }
+      expect( response.parsed_body ).to eq("No 'nicknames' key found in params")
+    end
+
+    it "returns an error if less than 3 players nicknames were given" do
+      users_nicknames = ["allen", "joe"]
+      post :create, params: { nicknames: users_nicknames }
+      expect( response.parsed_body ).to eq("#{users_nicknames.length} players provided, there must be 3..10 players")
+    end
+
+    it "returns an error if more than 10 players nicknames were given" do
+      users_nicknames = ("a".."z").to_a
+      post :create, params: { nicknames: users_nicknames }
+      expect( response.parsed_body ).to eq("#{users_nicknames.length} players provided, there must be 3..10 players")
+    end
+
+    it "excludes duplicate nicknames if any in the params" do
+      duplicate_users_nicknames = (["julia", "bob", "fiona"] * 2) # ruby == magic
+      post :create, params: { nicknames: duplicate_users_nicknames }
+      expect( response.parsed_body.length ).to eq(3)
+
+      response_users_nicknames = response.parsed_body.map { |user| user["nickname"] }
+      expect( response_users_nicknames ).to eq(duplicate_users_nicknames.uniq)
+    end
+  end
 end
