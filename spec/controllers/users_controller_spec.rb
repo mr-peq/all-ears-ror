9f93b83f3a811792d81467a5bb8809700c5fc162
Theodore Saulnier <theodore1712@yahoo.fr>
2024-02-13 18:42:49 +0100

    Added tests for users_controller#show & fixed bug in #show

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,14 +1,15 @@
 require "rails_helper"
 
 RSpec.describe UsersController, type: :controller do
-  let!(:allen) { create(:user, {nickname: 'allen'}) }
-  let!(:joe) { create(:user, {nickname: 'joe'}) }
-  let!(:sam) { create(:user, {nickname: 'sam'}) }
-  let!(:peq) { create(:user, {nickname: 'peq'}) }
+  let!(:allen) { create(:user, {nickname: "allen"}) }
+  let!(:joe) { create(:user, {nickname: "joe"}) }
+  let!(:sam) { create(:user, {nickname: "sam"}) }
+  let!(:peq) { create(:user, {nickname: "peq"}) }
 
-  describe 'GET#index' do
+  describe "GET#index" do
     it "returns a 200 HTTP status" do
       get :index
+      p response.parsed_body
       expect( response ).to have_http_status(200)
     end
 
@@ -28,6 +29,18 @@ RSpec.describe UsersController, type: :controller do
       response_users_nicknames = response.parsed_body.map { |user| user["nickname"] }
       expect( response_users_nicknames ).to eq(created_users_nicknames)
     end
+  end
 
+  describe "GET#show" do
+    it "returns the user with the provided nickname" do
+      get :show, params: { nickname: allen.nickname }
+      expect( response.parsed_body["nickname"] ).to eq(allen.nickname)
+    end
+
+    it "returns an error if no user was found with the provided nickname" do
+      not_a_user_nickname = "intruder"
+      get :show, params: { nickname: not_a_user_nickname }
+      expect( response.parsed_body ).to eq("User with nickname #{not_a_user_nickname} not found")
+    end
   end
 end
