ac4e78cb2e8f57d71c05c3a92be6f1312a315234
Theodore Saulnier <theodore1712@yahoo.fr>
2024-02-13 18:01:54 +0100

    Fixed PATCH#update test #2

--- spec/controllers/matches_controller_spec.rb
+++ spec/controllers/matches_controller_spec.rb
@@ -93,8 +93,8 @@ RSpec.describe MatchesController, type: :controller do
 
     it "returns an error if a user sent in params is not playing in this match" do
       id = match.id
-      patch :update, params: { id:, players: [{ nickname: "alt_user2", score: 777 }] }
-      expected_response = "Player with nickname [alt_user2] is not a participant of this match"
+      patch :update, params: { id:, players: [{ nickname: "peq", score: 777 }] }
+      expected_response = "Player with nickname [peq] is not a participant of this match"
       expect( response.body ).to eq(expected_response)
     end
 
